484301bd87e566448ebe3762af70dce081f0a2b5
Nardiéna Pratama <34739714+nardienapratama@users.noreply.github.com>
2023-03-01 03:39:07 +0000

    Configure db

--- todo/__init__.py
+++ todo/__init__.py
@@ -3,7 +3,22 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .views.routes import api
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+
+    # Load the models
+    from todo.models import db
+    from todo.models.todo import Todo
+    db.init_app(app)
+
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+    # Register the blueprints
+    from todo.views.routes import api
     app.register_blueprint(api)
+
+    
 
     return app
